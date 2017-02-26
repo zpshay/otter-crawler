21dbab5bd7b01c2373d0975d9d4014216fd60d30
ZWORK\Zachary <zshay@moresteam.com>
2017-02-26 16:07:39 -0500

    Spider dives into multiple links, next update will make it so it doesnt write same src in different link.

--- videoSpider/spiders/videocrawl.py
+++ videoSpider/spiders/videocrawl.py
@@ -2,10 +2,11 @@
 """
 Created on Fri Feb  3 11:35:25 2017
 
-@author: Zachary
+@author: Zachary Shay
 """
 
 import scrapy
+import json
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
@@ -17,16 +18,24 @@ class VideoCrawl(scrapy.Spider):
         'http://www.w3schools.com/html/html5_video.asp',
         'https://www.html5rocks.com/en/tutorials/video/basics/',
             ]
-
+            
     def parse(self, response):
+     # follow links to author pages
+        for href in response.css(' a::attr(href)').extract():
+            yield scrapy.Request(response.urljoin(href),
+                                 callback=self.parse_video)
+
+    def parse_video(self, response):
         for video in response.css('video'):
             http_exists = video.xpath('./source/@src').extract_first()[:4]
             if http_exists == "http":
                 full_url = video.xpath('./source/@src').extract_first()
-            
-                yield{
-                    'video': full_url
-                }
+                data = json.loads("video.json")
+                for dest in data['to']['data']:
+                    if 'id' not in dest:
+                        yield{
+                            'video': full_url
+                        }
             else:
                 video_url = str(video.xpath('./source/@src').extract_first())
                 count_path = video_url.count("../")
@@ -38,10 +47,12 @@ class VideoCrawl(scrapy.Spider):
                 translation_table = dict.fromkeys(map(ord, "]'["), None)
                 video_url = video_url.translate(translation_table)
                 full_url = delimeter_added_url + video_url
-                
-                yield{
-                    'video': full_url
-                }
+                data = json.loads("video.json")
+                for dest in data['to']['data']:
+                    if 'id' not in dest:
+                        yield{
+                            'video': full_url
+                        }
 
 if __name__ == "__main__":
     app.run()
